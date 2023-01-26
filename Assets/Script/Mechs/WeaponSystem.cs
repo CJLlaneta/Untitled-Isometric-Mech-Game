fa866e8f63d090279117c52e254a72d36f25bfd1
LAPTOP-DI1PRAG3\CJ <llaneta.christian@gmail.com>
2023-01-26 14:58:13 +0800

    Code improvement

--- Assets/Script/Mechs/WeaponSystem.cs
+++ Assets/Script/Mechs/WeaponSystem.cs
@@ -6,8 +6,8 @@ using System.Linq;
 public class WeaponSystem : MonoBehaviour
 {
     public List<WeaponHandler> weaponsHold;
-    private List<IWeapon> _weaponsRightPosition = new List<IWeapon>();
-    private List<IWeapon> _weaponsLeftPosition = new List<IWeapon>();
+    private List<IWeapon> _listRightArmWeapons = new List<IWeapon>();
+    private List<IWeapon> _listLeftArmWeapons = new List<IWeapon>();
 
 
     public bool FireWeapons(WeaponHandler.WeaponLocation WeaponLocation, 
@@ -15,30 +15,30 @@ public class WeaponSystem : MonoBehaviour
     {
         if (WeaponLocation == WeaponHandler.WeaponLocation.right)
         {
-           var _result = _weaponsRightPosition.Select(w => w.OnShoot(TargetAimPoint));
-            return _result.Any(r => r);
+           var hasfiredWeapon = _listRightArmWeapons.Select(w => w.OnShoot(TargetAimPoint));
+            return hasfiredWeapon.Any(result => result);
         }
         else if (WeaponLocation == WeaponHandler.WeaponLocation.left)
         {
-            var _result = _weaponsLeftPosition.Select(w => w.OnShoot(TargetAimPoint));
-            return _result.Any(r => r);
+            var hasfiredWeapon = _listLeftArmWeapons.Select(w => w.OnShoot(TargetAimPoint));
+            return hasfiredWeapon.Any(result => result);
         }
         return false;
     }
     public void InitializeProperties(GameObject owner)
     {
-        _weaponsRightPosition.Clear();
-        _weaponsLeftPosition.Clear();
+        _listRightArmWeapons.Clear();
+        _listLeftArmWeapons.Clear();
         foreach (WeaponHandler wh in weaponsHold)
         {
             wh.SetTheOwner(owner);
             if (wh.weaponLocation == WeaponHandler.WeaponLocation.right)
             {
-                _weaponsRightPosition.Add(wh);
+                _listRightArmWeapons.Add(wh);
             }
             else if (wh.weaponLocation == WeaponHandler.WeaponLocation.left)
             {
-                _weaponsLeftPosition.Add(wh);
+                _listLeftArmWeapons.Add(wh);
             }
         }
     }
