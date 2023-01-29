2b5cfcc2a157301fd1b48f7723b449bf7f50d1c6
LAPTOP-DI1PRAG3\CJ <llaneta.christian@gmail.com>
2023-01-29 18:39:00 +0800

    Crusher logic

--- Assets/Script/General/DamageAndDestruction/CrusherChecker.cs
+++ Assets/Script/General/DamageAndDestruction/CrusherChecker.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static DamageSystem;
 
 public class CrusherChecker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TagCollision _tagCollisions;
+    [SerializeField] float _crushMass = 100;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        _hittedObject = other.gameObject;
+        ApplyConditions(_hittedObject.tag);
     }
-
-    // Update is called once per frame
-    void Update()
+    GameObject _hittedObject;
+    private void ApplyDamage()
     {
-        
+        _hittedObject.GetComponent<DamageSystem>().ApplyDamage(_crushMass);
+    }
+    private void ApplyConditions(string tag)
+    {
+
+        CollisionTagProperties colProperties = _tagCollisions.CollisionTag.SingleOrDefault(col => col.TagName == tag);
+        if (colProperties !=null && colProperties.HasDamageSystem)
+        {
+            ApplyDamage();
+        }
+
     }
 }
